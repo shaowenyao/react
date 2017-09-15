2f8ec69c7b947478b3fc5038a66cd43887fb6019
Shaowen Yao <dsydesign@gmail.com>
2017-09-15 15:28:19 -0700

    router basic
    router basic

--- portfolio/App.js
+++ portfolio/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
-const websiteName = "ShaowenYao";
+const Header = () => (
+  <header>
+  <nav>
+  <ul>
+  <li><Link to="/">Home</Link></li>
+  <li><Link to="/about">About</Link></li>
+  <li><Link to="/work">Work</Link></li>
+  <li><Link to="/contact">Contact</Link></li>
+  </ul>
+  </nav>
+  </header>
+  )
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2> {websiteName} </h2>
-      </div>
-      <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a href="#" id="about">About</a>
-      </div>
-      );
-  }
-}
+const Main = () => (
+  <main>
+  <Switch>
+  <Route exact path="/" component={Home} />
+  <Route path = "/about" component={About} />
+  <Route path = "/work" component={Work} />
+  <Route path = "/contact" component={Contact} />
+  </Switch>
+  </main>
+  )
+
+const Home = () => (
+  <div>Homepage</div>
+  )
+
+const About = () => (
+  <div>About</div>
+  )
+
+const Work = () => (
+  <div>Work</div>
+  )
+
+const Contact = () => (
+  <div>Contact</div>
+  )
+const App = () => (
+  <div>
+  <Header />
+  <Main />
+  </div>
+  )
 
 export default App;

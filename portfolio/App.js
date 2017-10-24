4bf73c3d0e3773a0f48d3e6fcd33591564b94c37
Shaowen Yao <dsydesign@gmail.com>
2017-10-24 16:20:42 -0700

    final adj1

--- portfolio/App.js
+++ portfolio/App.js
@@ -56,7 +56,7 @@ const About_content = (
   <li>Actionscript + Flash Animation</li>
   <li>Media Production: Photography + Videography</li>
 
-  <h5>Freelance consulting | 09.2016-present</h5>
+  <h5>FREELANCE CONSULTING <br /> 09.2016-present</h5>
 
   <li>Identity design with small business clients to fit their own creative identity</li>
   <li>Web design + scalable development for business requirements with emphasis on mobile/responsive-first design</li>
@@ -65,7 +65,7 @@ const About_content = (
   <li>SEO optimization with new and existing website to improve traffic and conversion rate up to 50%</li>
   <li>Implement custom ReactJS customer facing applications for small businesses</li>
 
-  <h5>CITY & COUNTY OF SAN FRANCISCO | Mobile Developer | 06.2014-09.2016</h5>
+  <h5>CITY & COUNTY OF SAN FRANCISCO <br /> Mobile Developer <br /> 06.2014-09.2016</h5>
 
   <li>Mobile website development of user-friendly websites for the general public to be 100% responsive and usable</li>
   <li>Emphasis on cross-browser, ARIA, and other disabled user interface features for the general public to make UI accessible for all users</li>
@@ -73,7 +73,7 @@ const About_content = (
   <li>Designing UI/UX interactive flows for front-facing mobile and web initiatives to improve usability</li>
   <li>CMS development with Vision + Drupal CMS for internal government departments for website refresh</li>
 
-  <h5>PERFICIENT INC | Technical Consultant | 01.2013 - 03.2014</h5>
+  <h5>PERFICIENT INC <br /> Technical Consultant <br /> 01.2013 - 03.2014</h5>
 
   <li>Salesforce VisualForce development for custom client solutions</li>
   <li>UI development (CSS/HTML/JS) for custom client user interface (ex. MobileIron, Frontrange)</li>
@@ -83,7 +83,7 @@ const About_content = (
   <li>Media production (photography/videography) for new company branding</li>
   <li>Consultant on multiple projects applying expert knowledge of best design/coding practices from wireframe to full implementation</li>
 
-  <h5>COSTPLUS WORLDMARKET | Web Developer | 02.2011-06.2012</h5>
+  <h5>COSTPLUS WORLDMARKET <br /> Web Developer <br /> 02.2011-06.2012</h5>
 
   <li>Custom script development in Javascript/jQuery module development for seasonal campaign front page modules</li>
   <li>Integration of new technologies (HTML5/CSS3/jQuery) to modernize old templates into responsive and mobile-friendly layouts</li>
@@ -91,7 +91,7 @@ const About_content = (
   <li>CMS & eCommerce content maintenance (GSI commerce) and management</li>
   <li>Developing weekly marketing campaign assets such as emails, front page content, and seasonal campaigns in HTML/CSS/JS</li>
 
-  <h5>TELAVA NETWORKS | Web Developer | 06.2008 - 10.2010</h5>
+  <h5>TELAVA NETWORKS <br /> Web Developer <br /> 06.2008 - 10.2010</h5>
 
   <li>Implemented wireframes in XHTML, ASP.NET and CSS to improve user-centered design (UCD) efforts for numerous pages</li>
   <li>Implemented UI/UX for dynamic back-end content (eCommerce, sign-up, etc) to be user-friendly and follow company creative identity</li>

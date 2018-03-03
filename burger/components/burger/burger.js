81d5a7e1a27d69c4cc9040ae046e294729433033
dsydesign <dsydesign@gmail.com>
2018-03-03 14:46:14 -0800

    burger dynamic output

--- burger/components/burger/burger.js
+++ burger/components/burger/burger.js
@@ -3,11 +3,16 @@ import classes from './burger.css';
 import BurgerIngredient from './burgeringredient/burgeringredient';
 
 const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients)
+	.map( igKey => {
+		return [...Array( props.ingredients[igKey] )].map((_, i) => {
+			return <BurgerIngredient key={igKey+i} type={igKey} />
+		});
+	});
 	return (
 		<div className={classes.Burger}>
 		<BurgerIngredient type ="bread-top" />
-		<BurgerIngredient type ="meat" />
-		<BurgerIngredient type ="cheese" />
+		{transformedIngredients}
 		<BurgerIngredient type ="bread-bottom" />
 		</div>
 		);
--- burger/containers/burgerbuilder/burgerbuilder.js
+++ burger/containers/burgerbuilder/burgerbuilder.js
@@ -3,10 +3,18 @@ import Auxdiv from '../../hoc/auxdiv';
 import Burger from '../../components/burger/burger'
 
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 1,
+			cheese: 2,
+			meat: 2
+		}
+	}
 	render () {
 		return(
 			<Auxdiv>
-			<Burger />
+			<Burger ingredients={this.state.ingredients} />
 			<div>Build Controls </div>
 			</Auxdiv>
 			);

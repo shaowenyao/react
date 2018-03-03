b3eb72e7025ccf76900913d8a7d5ada1bf987828
dsydesign <dsydesign@gmail.com>
2018-03-03 15:58:08 -0800

    burger add button func

--- burger/components/burger/buildcontrols/buildcontrols.js
+++ burger/components/burger/buildcontrols/buildcontrols.js
@@ -4,16 +4,16 @@ import BuildControl from './buildcontrol/buildcontrol';
 
 const controls = [
 { label: 'Salad', type: 'salad' },
-{ label: 'Bacon', type: 'Bacon' },
+{ label: 'Bacon', type: 'bacon' },
 { label: 'Cheese', type: 'cheese' },
-{ label: 'Meat', type: 'meat' },
+{ label: 'Meat', type: 'meat' }
 ];
 
 const buildControls = (props) => {
 	return (
 		<div className={classes.BuildControls}>
 		{controls.map( ctrl =>
-			<BuildControl key={ctrl.label} label={ctrl.label} />
+			<BuildControl key={ctrl.label} label={ctrl.label} added={ () => props.ingredientAdded(ctrl.type)} />
 			)}
 		</div>
 		);

7c1f3a8c193ed36056a3dee02f1a35630237ba72
dsydesign <dsydesign@gmail.com>
2018-03-03 22:14:43 -0800

    burger btn fix

--- burger/components/UI/button/button.js
+++ burger/components/UI/button/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './button.css';
 
 const button = (props) => {
-	return <button className={[classes.Button, classes[props.btnType]].join('')} onClick={props.clicked} >{props.children}</button>
+	return <button className={[classes.Button, classes[props.btnType]].join(' ')} onClick={props.clicked} >{props.children}</button>
 }
 
 export default button;

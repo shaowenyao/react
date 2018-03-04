77b113ab49d2929dc41bdaa8db326258aa6a3ae4
dsydesign <dsydesign@gmail.com>
2018-03-03 18:08:31 -0800

    burger modal show hide

--- burger/containers/burgerbuilder/burgerbuilder.js
+++ burger/containers/burgerbuilder/burgerbuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
 			meat: 0
 		},
 		totalPrice: 4,
-		purchaseable: false
+		purchaseable: false,
+		purchasing: false
 	}
 
 	updatePurchaseState (ingredients) {
@@ -63,6 +64,11 @@ class BurgerBuilder extends Component {
 		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 		this.updatePurchaseState(updatedIngredients);
 	}
+
+	purchaseHandler = () => {
+		this.setState( { purchasing: true })
+	}
+
 	render () {
 		const disabledInfo = { ...this.state.ingredients }
 		for (let key in disabledInfo){
@@ -70,13 +76,16 @@ class BurgerBuilder extends Component {
 		}
 		return(
 			<Auxdiv>
-			<Modal><OrderSummary ingredients={this.state.ingredients} /></Modal>
+			<Modal show={this.state.purchasing} >
+			<OrderSummary ingredients={this.state.ingredients} /><
+			/Modal>
 			<Burger ingredients={this.state.ingredients} />
 			<BuildControls
 			ingredientAdded={this.addIngredientHanlder} 
 			ingredientRmoved = { this.removeIngredientHanlder} 
 			disabled={disabledInfo} 
 			purchaseable={this.state.purchaseable}
+			ordered={this.purchaseHandler}
 			price = {this.state.totalPrice}
 			/>
 			</Auxdiv>

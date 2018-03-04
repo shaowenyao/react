7a13f798587860c45c4991d80eff57989299a278
dsydesign <dsydesign@gmail.com>
2018-03-03 17:37:39 -0800

    burger order btn

--- burger/containers/burgerbuilder/burgerbuilder.js
+++ burger/containers/burgerbuilder/burgerbuilder.js
@@ -18,7 +18,19 @@ class BurgerBuilder extends Component {
 			cheese: 0,
 			meat: 0
 		},
-		totalPrice: 4
+		totalPrice: 4,
+		purchaseable: false
+	}
+
+	updatePurchaseState (ingredients) {
+		const sum = Object.keys(ingredients)
+		.map(igKey => {
+			return ingredients[igKey];
+		})
+		.reduce((sum, el) => {
+			return sum + el;
+		}, 0);
+		this.setState({ purchaseable: sum>0});
 	}
 	
 	addIngredientHanlder = (type) => {
@@ -32,6 +44,7 @@ class BurgerBuilder extends Component {
 		const oldPrice = this.state.totalPrice;
 		const newPrice = oldPrice + priceAddition;
 		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+		this.updatePurchaseState(updatedIngredients);
 	}
 
 	removeIngredientHanlder = (type) => {
@@ -46,7 +59,7 @@ class BurgerBuilder extends Component {
 		const oldPrice = this.state.totalPrice;
 		const newPrice = oldPrice - priceDeduction;
 		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
+		this.updatePurchaseState(updatedIngredients);
 	}
 	render () {
 		const disabledInfo = { ...this.state.ingredients }
@@ -60,6 +73,7 @@ class BurgerBuilder extends Component {
 			ingredientAdded={this.addIngredientHanlder} 
 			ingredientRmoved = { this.removeIngredientHanlder} 
 			disabled={disabledInfo} 
+			purchaseable={this.state.purchaseable}
 			price = {this.state.totalPrice}
 			/>
 			</Auxdiv>

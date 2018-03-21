55a34c626c55bbff70f119f0607380fd8d436fd2
dsydesign <dsydesign@gmail.com>
2018-03-20 17:18:03 -0700

    router basic

--- burger/App.js
+++ burger/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Layout from './hoc/layout/layout';
 import BurgerBuilder from './containers/burgerbuilder/burgerbuilder';
+import Checkout from './containers/checkout/checkout';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 			<Layout>
-			<BurgerBuilder />
+			<Switch>
+			<Route path='/' exact component={BurgerBuilder} />
+			<Route path='/checkout' component={Checkout} />
+			</Switch>
 			</Layout>
 			</div>
 			);

09b12c9ba23a29f60dda90e97ce7186d3ad67655
dsydesign <dsydesign@gmail.com>
2018-04-17 19:01:42 -0700

    burger redux p1 done

--- burger/containers/burgerbuilder/burgerbuilder.js
+++ burger/containers/burgerbuilder/burgerbuilder.js
@@ -19,7 +19,6 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
 	state = {
-		purchaseable: false,
 		purchasing: false,
 		loading: false,
 		error: null
@@ -43,7 +42,7 @@ class BurgerBuilder extends Component {
 		.reduce((sum, el) => {
 			return sum + el;
 		}, 0);
-		this.setState({ purchaseable: sum>0});
+		return sum>0;
 	}
 	
 
@@ -56,18 +55,7 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseContinueHandler = () => {
-		//alert("continue");
-		
-		const queryParams = [];
-		for (let i in this.state.ingredients){
-			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-		}
-		queryParams.push('price=' + this.state.totalPrice);
-		const queryString = queryParams.join('&');
-		this.props.history.push({
-			pathname: '/checkout',
-			search: '?' + queryString
-		});
+		this.props.history.push('/checkout');
 	}
 	render () {
 		const disabledInfo = { ...this.props.ings }
@@ -84,7 +72,7 @@ class BurgerBuilder extends Component {
 				ingredientAdded={this.props.onIngredientAdded} 
 				ingredientRmoved = { this.props.onIngredientRemoved} 
 				disabled={disabledInfo} 
-				purchaseable={this.state.purchaseable}
+				purchaseable={this.updatePurchaseState(this.props.ings)}
 				ordered={this.purchaseHandler}
 				price = {this.props.price}
 				/>
--- burger/containers/checkout/checkout.js
+++ burger/containers/checkout/checkout.js
@@ -2,43 +2,33 @@ import React, {Component} from 'react';
 import CheckoutSummary from '../../components/order/checkoutsummary/checkoutsummary';
 import { Route } from 'react-router-dom';
 import ContactData from './contactdata/contactdata';
+import { connect } from 'react-redux';
 
 class checkout extends Component {
-	state = {
-		ingredients: null,
-		price: 0
-	}
-
-	componentWillMount() {
-		const query = new URLSearchParams(this.props.location.search);
-		const ingredients = {};
-		let price =0;
-		for (let param of query.entries()) {
-			if (param[0] === 'price') { 
-				price = param[1];
-			} else {
-				ingredients[param[0]] = +param[1];
-			}
-		}
-		this.setState({ingredients: ingredients, totalPrice:price});
-	}
 
 	checkoutCancelledHanlder = () => {
 		this.props.history.goBack();
 	}
+
 	checkoutContinuedHandler = () => {
 		this.props.history.replace('/checkout/contact-data');
 	}
 	render() {
 		return (
 			<div>
-			<CheckoutSummary ingredients={this.state.ingredients} 
+			<CheckoutSummary ingredients={this.props.ings} 
 			checkoutCancelled={this.checkoutCancelledHanlder} 
 			checkoutContinued={this.checkoutContinuedHandler} />
-			<Route path={this.props.match.path + '/contact-data'} render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
+			<Route path={this.props.match.path + '/contact-data'} component={ContactData} />
 			</div>
 			);
 	}
 }
 
-export default checkout;
+const mapStateToProps = state => {
+	return {
+		ings: state.ingredients
+	}
+};
+
+export default connect(mapStateToProps)(checkout);

b10552e8bf3b628027d5c7c6df17301437ff3ae1
Shaowen Yao <dsydesign@gmail.com>
2018-04-20 15:09:08 -0700

    burger redux extension

--- burger/index.js
+++ burger/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore} from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
 import reducer from './store/reducer';
+import { createStore } from 'redux';
 
-const store = createStore(reducer);
+const store = createStore(
+	reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	);
+
 
 const app = (
 	<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>

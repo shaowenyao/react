bcb7d21b0fe78d19e5c9840dde019853f9d34daa
Shaowen Yao <dsydesign@gmail.com>
2018-04-24 13:55:19 -0700

    Revert "burger redux extension"
    This reverts commit b10552e8bf3b628027d5c7c6df17301437ff3ae1.

--- burger/index.js
+++ burger/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+<<<<<<< HEAD
 import { createStore } from 'redux';
 
+=======
+import { createStore} from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+>>>>>>> parent of b10552e... burger redux extension
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';

f89f64dedfb397e57304591742a6c59743d0e305
Shaowen Yao <dsydesign@gmail.com>
2019-01-30 17:12:41 -0800

    index redux fix

--- portfolio/App.js
+++ portfolio/App.js
@@ -6,38 +6,23 @@ import Footer from './components/footer';
 import { MainRouter } from './components/mainRouter';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { render } from 'react-dom'
 import store from "./store/index";
 import { addArticle } from "./actions/index";
 
 window.store = store;
 window.addArticle = addArticle;
 
-class ExampleComponent extends Component {
-  constructor() {
-    super();
-    this.state = {
-      articles: [
-        { title: "React Redux Tutorial for Beginners", id: 1 },
-        { title: "Redux e React: cos'è Redux e come usarlo con React", id: 2 }
-      ]
-    };
-  }
-  render() {
-    const { articles } = this.state;
-    return <ul>{articles.map(el => <li key={el.id}>{el.title}</li>)}</ul>;
-  }
-}
-
 const App = () => (
+  <div>
+  
   <BrowserRouter>
     <div>
-      <ExampleComponent></ExampleComponent>
       <Header />
       <MainRouter />
       { Footer }
     </div>
   </BrowserRouter>
+  </div>
   )
 
 export default App;

b4e5e0df87145ceb2cda1e830970036e5498f8d4
Shaowen Yao <dsydesign@gmail.com>
2019-02-01 16:43:18 -0800

    redux form p1

--- portfolio/App.js
+++ portfolio/App.js
@@ -6,15 +6,33 @@ import Footer from './components/footer';
 import { MainRouter } from './components/mainRouter';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
 import store from "./store/index";
 import { addArticle } from "./actions/index";
+import ReduxList from './reduxcomponents/list.jsx';
+import ReduxForm from './reduxcomponents/form.jsx';
 
 window.store = store;
 window.addArticle = addArticle;
 
+const ReduxListContainer = () => (
+  <div className="row mt-5">
+    <div className="col-md-4 offset-md-1">
+      <h2>Articles</h2>
+      <ReduxList />
+    </div>
+    <div className="col-md-4 offset-md-1">
+      <h2>Add a new article</h2>
+      <ReduxForm />
+    </div>
+  </div>
+); 
+
 const App = () => (
   <div>
-  
+  <Provider store={store}>
+    <ReduxListContainer />
+  </Provider>
   <BrowserRouter>
     <div>
       <Header />
